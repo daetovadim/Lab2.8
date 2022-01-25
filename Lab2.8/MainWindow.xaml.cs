591a07af9118385d3a6d63a1739ba14aff56e3a1
Вадим Малутин <malutin.vadim@yandex.ru>
2022-01-25 12:43:09 +0600

    Add project files.

---
+++ Lab2.8/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//Доработать проект текстового редактора из задания 7, добавив словарь ресурсов (в виде файла) со списками названий шрифтов и размеров.
+
+namespace Lab2._8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void fontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = (sender as ComboBox).SelectedItem as string;
+            if (richTB != null)
+                richTB.FontFamily = new FontFamily(fontName);
+        }
+
+        private void fontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            double fontSize = Convert.ToDouble((sender as ComboBox).SelectedItem.ToString());
+            if (richTB != null)
+                richTB.FontSize = fontSize;
+        }
+
+        private void fontColor_Checked(object sender, RoutedEventArgs e)
+        {
+            richTB.Foreground = blackColor.IsChecked.Value ? Brushes.Black : Brushes.Red;
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Rich Text Format (*.rtf)|*.rtf|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(openFileDialog.FileName, FileMode.Open);
+                TextRange range = new TextRange(richTB.Document.ContentStart, richTB.Document.ContentEnd);
+                range.Load(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Rich Text Format (*.rtf)|*.rtf|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(saveFileDialog.FileName, FileMode.Create);
+                TextRange range = new TextRange(richTB.Document.ContentStart, richTB.Document.ContentEnd);
+                range.Save(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        private void CloseExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+
+        }
+    }
+}
